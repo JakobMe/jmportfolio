55f66c2da171ab952ed57386b07dfc687eea9db9
Jakob Metzger <jakob.me@gmail.com>
2015-11-09 01:27:30 +0100

    Added some test content and elaborated on the terminal styling.

--- index.php
+++ index.php
@@ -26,8 +26,89 @@
     	<!--Terminal-->
     	<article id="terminal">
         	
-        	<i>jmportfolio</i>:~ <b>guest</b>$ <span class="cursor blink">&nbsp;</span>
+        	<!--Welcome-->
+        	<p class="welcome">
+            	Welcome to <b>jmportfolio</b>.de!<br/>
+            	This is Jakob Metzger's personal online portfolio.
+        	</p>
         	
+        	<!--Introduction-->
+        	<p class="introduction">
+            	Press <dfn>left</dfn> and <dfn>right</dfn> arrow keys to
+            	move the cursor.<br/>
+            	Press <dfn>up</dfn> and <dfn>down</dfn> arrow keys
+            	to navigate your command history.<br/>
+            	Press <dfn>tab</dfn> to autocomplete filenames.<br/>
+            	Press <dfn>enter</dfn> to execute your <i>command</i>.
+        	</p>
+        	
+        	<!--Hint-->
+        	<p class="hint">
+            	<u>Hint:</u> Use the <i>help</i> command to list all available commands.
+        	</p>
+        	
+        	<!--Command: help-->
+        	<p class="command">
+            	<span class="prompt"><b>jmportfolio</b>:~ <u>guest$</u> help</span>
+        	</p>
+        	
+        	<!--Output: help-->
+        	<p class="output">
+            	<i>ls</i> — List files<br/>
+            	<i>cd</i> — Change directory<br/>
+            	<i>cat</i> — Display file content<br/>
+            	<i>help</i> — Display this help<br/>
+            	<i>mail</i> — Send a message<br/>
+            	<i>clear</i> — Clear terminal<br/>
+            	<i>notice</i> — Display legal information<br/>
+            	<br/>
+            	<u>hint:</u> Type your <i>command</i> below and press <dfn>enter</dfn> to execute it.
+        	</p>
+        	
+        	<!--Command: ssh-->
+        	<p class="command">
+            	<span class="prompt"><b>jmportfolio</b>:~ <u>guest$</u> ssh</span>
+        	</p>
+        	
+        	<!--Output: ssh-->
+        	<p class="output">
+            	ssh: <strong>Command not found</strong>. Use <i>help</i>
+            	to list available <i>commands</i>.<br/>
+        	</p>
+        	
+        	<!--Command: ls-->
+        	<p class="command">
+            	<span class="prompt"><b>jmportfolio</b>:~ <u>guest$</u> ls</span>
+        	</p>
+        	
+        	<!--Output: ls-->
+        	<p class="output">
+            	<dfn>project01.jpg</dfn><br/>
+            	<dfn>project02.jpg</dfn><br/>
+            	<dfn>project03.jpg</dfn><br/>
+            	<dfn>project04.jpg</dfn><br/>
+            	<dfn>project05.jpg</dfn><br/>
+            	<dfn>project06.jpg</dfn><br/>
+            	<dfn>project07.jpg</dfn>
+        	</p>
+        	
+        	<!--Command-->
+        	<p class="command">
+            	<span class="prompt"><b>jmportfolio</b>:~ <u>guest$</u> cat</span>
+        	</p>
+        	
+        	<!--Output-->
+        	<p class="output">
+            	cat: <strong>You must specify a file to display</strong>.<br/>
+            	<u>usage:</u> <i>cat</i> <dfn>filename</dfn>
+        	</p>
+        	
+        	<!--Command-->
+        	<p class="command">
+            	<span class="prompt"><b>jmportfolio</b>:~ <u>guest$</u></span>
+            	<span class="cursor blink">&nbsp;</span>
+        	</p>
+
     	</article>
     	
 	</body>
