ac15e599b14c981173fa51197c9348e821f1bbe1
Jakob Metzger <jakob.me@gmail.com>
2015-11-11 11:07:57 +0100

    Replaced favicons and logo.

--- index.php
+++ index.php
@@ -12,28 +12,28 @@
 		<meta charset="utf-8">
 		
 		<!--Favicon-->
-		<link rel="apple-touch-icon" sizes="57x57" href="img/favicon/apple-touch-icon-57x57.png">
-        <link rel="apple-touch-icon" sizes="60x60" href="img/favicon/apple-touch-icon-60x60.png">
-        <link rel="apple-touch-icon" sizes="72x72" href="img/favicon/apple-touch-icon-72x72.png">
-        <link rel="apple-touch-icon" sizes="76x76" href="img/favicon/apple-touch-icon-76x76.png">
-        <link rel="apple-touch-icon" sizes="114x114" href="img/favicon/apple-touch-icon-114x114.png">
-        <link rel="apple-touch-icon" sizes="120x120" href="img/favicon/apple-touch-icon-120x120.png">
-        <link rel="apple-touch-icon" sizes="144x144" href="img/favicon/apple-touch-icon-144x144.png">
-        <link rel="apple-touch-icon" sizes="152x152" href="img/favicon/apple-touch-icon-152x152.png">
-        <link rel="apple-touch-icon" sizes="180x180" href="img/favicon/apple-touch-icon-180x180.png">
-        <link rel="icon" type="image/png" href="img/favicon/favicon-32x32.png" sizes="32x32">
-        <link rel="icon" type="image/png" href="img/favicon/favicon-194x194.png" sizes="194x194">
-        <link rel="icon" type="image/png" href="img/favicon/favicon-96x96.png" sizes="96x96">
-        <link rel="icon" type="image/png" href="img/favicon/android-chrome-192x192.png" sizes="192x192">
-        <link rel="icon" type="image/png" href="img/favicon/favicon-16x16.png" sizes="16x16">
-        <link rel="manifest" href="img/favicon/manifest.json">
-        <link rel="mask-icon" href="img/favicon/safari-pinned-tab.svg" color="#57b76e">
-        <link rel="shortcut icon" href="img/favicon/favicon.ico">
+		<link rel="apple-touch-icon" sizes="57x57" href="img/favicon/apple-touch-icon-57x57.png?v=XBBvRp7Q03">
+        <link rel="apple-touch-icon" sizes="60x60" href="img/favicon/apple-touch-icon-60x60.png?v=XBBvRp7Q03">
+        <link rel="apple-touch-icon" sizes="72x72" href="img/favicon/apple-touch-icon-72x72.png?v=XBBvRp7Q03">
+        <link rel="apple-touch-icon" sizes="76x76" href="img/favicon/apple-touch-icon-76x76.png?v=XBBvRp7Q03">
+        <link rel="apple-touch-icon" sizes="114x114" href="img/favicon/apple-touch-icon-114x114.png?v=XBBvRp7Q03">
+        <link rel="apple-touch-icon" sizes="120x120" href="img/favicon/apple-touch-icon-120x120.png?v=XBBvRp7Q03">
+        <link rel="apple-touch-icon" sizes="144x144" href="img/favicon/apple-touch-icon-144x144.png?v=XBBvRp7Q03">
+        <link rel="apple-touch-icon" sizes="152x152" href="img/favicon/apple-touch-icon-152x152.png?v=XBBvRp7Q03">
+        <link rel="apple-touch-icon" sizes="180x180" href="img/favicon/apple-touch-icon-180x180.png?v=XBBvRp7Q03">
+        <link rel="icon" type="image/png" href="img/favicon/favicon-32x32.png?v=XBBvRp7Q03" sizes="32x32">
+        <link rel="icon" type="image/png" href="img/favicon/favicon-194x194.png?v=XBBvRp7Q03" sizes="194x194">
+        <link rel="icon" type="image/png" href="img/favicon/favicon-96x96.png?v=XBBvRp7Q03" sizes="96x96">
+        <link rel="icon" type="image/png" href="img/favicon/android-chrome-192x192.png?v=XBBvRp7Q03" sizes="192x192">
+        <link rel="icon" type="image/png" href="img/favicon/favicon-16x16.png?v=XBBvRp7Q03" sizes="16x16">
+        <link rel="manifest" href="img/favicon/manifest.json?v=XBBvRp7Q03">
+        <link rel="mask-icon" href="img/favicon/safari-pinned-tab.svg?v=XBBvRp7Q03" color="#57b76e">
+        <link rel="shortcut icon" href="img/favicon/favicon.ico?v=XBBvRp7Q03">
         <meta name="apple-mobile-web-app-title" content="jmportfolio">
         <meta name="application-name" content="jmportfolio">
         <meta name="msapplication-TileColor" content="#57b76e">
-        <meta name="msapplication-TileImage" content="img/favicon/mstile-144x144.png">
-        <meta name="msapplication-config" content="img/favicon/browserconfig.xml">
+        <meta name="msapplication-TileImage" content="img/favicon/mstile-144x144.png?v=XBBvRp7Q03">
+        <meta name="msapplication-config" content="img/favicon/browserconfig.xml?v=XBBvRp7Q03">
         <meta name="theme-color" content="#57b76e">
 		
 		<!--Assets-->
