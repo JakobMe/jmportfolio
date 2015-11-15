775c9f8d71b6542a16648dae4393d75b60b74192
Jakob Metzger <jakob.me@gmail.com>
2015-11-15 17:10:33 +0100

    Fixed bugs in lightbox in input prompt.

--- index.php
+++ index.php
@@ -7,33 +7,33 @@
         <meta name="description" content="Jakob Metzger's online portfolio">
         <meta name="keywords" content="jakob, metzger, portfolio, frontend, developer, design, graphic">
 		<meta charset="utf-8">
-		<link rel="apple-touch-icon" sizes="57x57" href="img/favicon/apple-touch-icon-57x57.png?v=3">
-        <link rel="apple-touch-icon" sizes="60x60" href="img/favicon/apple-touch-icon-60x60.png?v=3">
-        <link rel="apple-touch-icon" sizes="72x72" href="img/favicon/apple-touch-icon-72x72.png?v=3">
-        <link rel="apple-touch-icon" sizes="76x76" href="img/favicon/apple-touch-icon-76x76.png?v=3">
-        <link rel="apple-touch-icon" sizes="114x114" href="img/favicon/apple-touch-icon-114x114.png?v=3">
-        <link rel="apple-touch-icon" sizes="120x120" href="img/favicon/apple-touch-icon-120x120.png?v=3">
-        <link rel="apple-touch-icon" sizes="144x144" href="img/favicon/apple-touch-icon-144x144.png?v=3">
-        <link rel="apple-touch-icon" sizes="152x152" href="img/favicon/apple-touch-icon-152x152.png?v=3">
-        <link rel="apple-touch-icon" sizes="180x180" href="img/favicon/apple-touch-icon-180x180.png?v=3">
-        <link rel="icon" type="image/png" href="img/favicon/favicon-32x32.png?v=3" sizes="32x32">
-        <link rel="icon" type="image/png" href="img/favicon/favicon-194x194.png?v=3" sizes="194x194">
-        <link rel="icon" type="image/png" href="img/favicon/favicon-96x96.png?v=3" sizes="96x96">
-        <link rel="icon" type="image/png" href="img/favicon/android-chrome-192x192.png?v=3" sizes="192x192">
-        <link rel="icon" type="image/png" href="img/favicon/favicon-16x16.png?v=3" sizes="16x16">
-        <link rel="manifest" href="img/favicon/manifest.json?v=3">
-        <link rel="mask-icon" href="img/favicon/safari-pinned-tab.svg?v=3" color="#57b76e">
-        <link rel="shortcut icon" href="img/favicon/favicon.ico?v=3">
+		<link rel="apple-touch-icon" sizes="57x57" href="img/favicon/apple-touch-icon-57x57.png?v=4">
+        <link rel="apple-touch-icon" sizes="60x60" href="img/favicon/apple-touch-icon-60x60.png?v=4">
+        <link rel="apple-touch-icon" sizes="72x72" href="img/favicon/apple-touch-icon-72x72.png?v=4">
+        <link rel="apple-touch-icon" sizes="76x76" href="img/favicon/apple-touch-icon-76x76.png?v=4">
+        <link rel="apple-touch-icon" sizes="114x114" href="img/favicon/apple-touch-icon-114x114.png?v=4">
+        <link rel="apple-touch-icon" sizes="120x120" href="img/favicon/apple-touch-icon-120x120.png?v=4">
+        <link rel="apple-touch-icon" sizes="144x144" href="img/favicon/apple-touch-icon-144x144.png?v=4">
+        <link rel="apple-touch-icon" sizes="152x152" href="img/favicon/apple-touch-icon-152x152.png?v=4">
+        <link rel="apple-touch-icon" sizes="180x180" href="img/favicon/apple-touch-icon-180x180.png?v=4">
+        <link rel="icon" type="image/png" href="img/favicon/favicon-32x32.png?v=4" sizes="32x32">
+        <link rel="icon" type="image/png" href="img/favicon/favicon-194x194.png?v=4" sizes="194x194">
+        <link rel="icon" type="image/png" href="img/favicon/favicon-96x96.png?v=4" sizes="96x96">
+        <link rel="icon" type="image/png" href="img/favicon/android-chrome-192x192.png?v=4" sizes="192x192">
+        <link rel="icon" type="image/png" href="img/favicon/favicon-16x16.png?v=4" sizes="16x16">
+        <link rel="manifest" href="img/favicon/manifest.json?v=4">
+        <link rel="mask-icon" href="img/favicon/safari-pinned-tab.svg?v=4" color="#57b76e">
+        <link rel="shortcut icon" href="img/favicon/favicon.ico?v=4">
         <meta name="apple-mobile-web-app-title" content="jmportfolio">
         <meta name="application-name" content="jmportfolio">
         <meta name="msapplication-TileColor" content="#57b76e">
-        <meta name="msapplication-TileImage" content="img/favicon/mstile-144x144.png?v=3">
-        <meta name="msapplication-config" content="img/favicon/browserconfig.xml?v=3">
+        <meta name="msapplication-TileImage" content="img/favicon/mstile-144x144.png?v=4">
+        <meta name="msapplication-config" content="img/favicon/browserconfig.xml?v=4">
         <meta name="theme-color" content="#57b76e">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Code+Pro:400" type="text/css" />
         <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-        <link rel="stylesheet" href="css/style.min.css?v=3" type="text/css" />
-        <script type="text/javascript" src="js/scripts.min.js?v=3"></script>
+        <link rel="stylesheet" href="css/style.min.css?v=4" type="text/css" />
+        <script type="text/javascript" src="js/scripts.min.js?v=4"></script>
 	</head>
 	<body>
     	<div id="terminal">
@@ -65,7 +65,7 @@
     	</div>
     	<div id="navigation">
         	<ul>
-            	<li><b id="type-mobile"><span>type</span></b></li>
+            	<li><b id="type-mobile"><span>Type</span></b></li>
         	</ul>
         	<form method="post" action="#">
             	<input type="text" id="input-mobile">
